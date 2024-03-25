c8565e220578ae5f5c2bb22350f61125ab5b7ff2
reallducky <157430079+reallducky@users.noreply.github.com>
2024-03-25 19:02:41 -0400

    Update mpgRfile.R

--- mpgRfile.R
+++ mpgRfile.R
@@ -3,3 +3,11 @@ Yi-hung, Lu
 library(tidyverse)
 head(mpg)
 summary(mpg$displ)
+mpg
+
+ggplot(data = mpg) +
+  geom_boxplot(mapping = aes(x = drv, y = displ, fill = drv)) +
+  ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
+  xlab("transmission types") +
+  ylab("displacement") 
+  
